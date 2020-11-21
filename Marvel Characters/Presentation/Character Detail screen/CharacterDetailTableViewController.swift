0c25b33207afe28ed211d1ccf09d71ffa57827e9
Daniel Illescas Romero <illescas.daniel@protonmail.com>
2020-11-21 20:09:31 +0100

    basic character detail info

--- Marvel Characters/Presentation/Character Detail screen/CharacterDetailTableViewController.swift
+++ Marvel Characters/Presentation/Character Detail screen/CharacterDetailTableViewController.swift
@@ -9,20 +9,25 @@ import UIKit
 
 class CharacterDetailTableViewController: UITableViewController {
 	
+	@IBOutlet private weak var bigCharacterNameLabel: UILabel!
+	@IBOutlet private weak var characterNameLabel: UILabel!
+	@IBOutlet private weak var characterDescriptionLabel: UILabel!
+	@IBOutlet private weak var characterLinksTextView: UITextView!
+	
 	private var character: MarvelCharacter!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
 		
-		self.title = nil
-		self.navigationItem.prompt = Other.attributionText
-    }
+		// Uncomment the following line to preserve selection between presentations
+		// self.clearsSelectionOnViewWillAppear = false
+		
+		// Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+		// self.navigationItem.rightBarButtonItem = self.editButtonItem
+		
+		setupNavigationBar()
+		setupCharacterViews()
+	}
 	
 	static func build(withCharacter character: MarvelCharacter) -> CharacterDetailTableViewController {
 		let storyboard = UIStoryboard(name: "Main", bundle: .main)
@@ -34,72 +39,29 @@ class CharacterDetailTableViewController: UITableViewController {
 	func setup(withCharacter character: MarvelCharacter) {
 		self.character = character
 	}
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	
+	// MARK: Convenience
+	
+	private func setupNavigationBar() {
+		self.title = nil
+		self.navigationItem.prompt = Other.attributionText
+		
+		self.navigationController?.navigationBar.barTintColor = self.view.backgroundColor
+		self.navigationController?.navigationBar.shadowImage = UIImage()
+		self.navigationController?.navigationBar.layoutIfNeeded()
+	}
+	
+	private func setupCharacterViews() {
+		let firstNameWords = character.name.split(separator: " ").prefix(2)
+		if firstNameWords.contains(where: { $0.contains("(") }) {
+			self.bigCharacterNameLabel.text = String(firstNameWords[0])
+		} else {
+			self.bigCharacterNameLabel.text = firstNameWords.joined(separator: " ")
+		}
+		self.characterNameLabel.text = character.name
+		self.characterDescriptionLabel.text = character.description.isEmpty ? "(empty)" : character.description
+		self.characterLinksTextView.text = character.urls.joined(separator: "\n\n")
+		
+		
+	}
 }
